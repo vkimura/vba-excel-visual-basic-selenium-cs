341ef3bfd4bc23f6d479b21f1d247148f3504079
Victor Kimura <vkimura@users.noreply.github.com>
2022-08-31 16:33:19 -0700

    escape quotes in SQL string for VBA Excel

--- vba-for-sql-insert-update.vb
+++ vba-for-sql-insert-update.vb
@@ -1,54 +1,29 @@
-Sub ForEachCell()
-    Dim Cell As Range
-    Dim Result() As String
-    Dim URL() As String
-    Dim Count As Integer
-    Dim ConcatenatedUrl, LastChar As String
-    Dim lastRow, i As Long
+Sub ConnectSQLRedirects()
+    Dim connection As ADODB.connection
+    Set connection = New ADODB.connection
+    Let server_name = "D117330\MSSQLSERVER02"
+    Let database_name = "CDICollege"
     
-    lastRow = Cells(Rows.Count, 1).End(xlUp).Row
+    Dim sqlQuery As String
+    sqlQuery = "SELECT * FROM [CDICollege].[dbo].[Redirects] WHERE [To] LIKE '%/study-on-campus/%/admissions/%'"
+    'sqlQuery = "SELECT *  FROM [CDICollege].[dbo].[Redirects] Where RedirectID = 2;"
     
-    For Each Cell In Sheets("404 CDICollege English FinalSql").Range("D7:D50")
-        Result() = Split(Cell.Value, "//")
-        UrlArr = Split(Result(1), "/")
-        Count = UBound(UrlArr)
-        ConcatenatedUrl = ""
-        
-        'create url
-        For i = 1 To Count
-            ConcatenatedUrl = ConcatenatedUrl & "/" & UrlArr(i)
-        Next i
-        
-        LastChar = Right(ConcatenatedUrl, 1)
-        
-        'only append if Count = 1 (i.e. there is only one part of URL) and the last character does not already have "/" at end for edge case when Cell.Value = https://www.cdicollege.ca/
-        If Count = 1 And LastChar <> "/" Then
-            ConcatenatedUrl = ConcatenatedUrl & "/" 'append last forward slash
-        ElseIf Count > 1 And LastChar <> "/" Then
-            ConcatenatedUrl = ConcatenatedUrl & "/" 'append last forward slash
-        End If
-        
-        'If Count = 1 Then
-        '    ConcatenatedUrl = "/" + UrlArr(0) + "/" + UrlArr(1) + "/"
-        'ElseIf Count = 2 Then
-        '    ConcatenatedUrl = "/" + UrlArr(0) + "/" + UrlArr(1) + "/" + UrlArr(2) + "/"
-        'ElseIf Count = 3 Then
-        '    ConcatenatedUrl = "/" + UrlArr(0) + "/" + UrlArr(1) + "/" + UrlArr(2) + "/" + UrlArr(3) + "/"
-        'End If
-        'If Count = 1 Then
-        '    ConcatenatedUrl = "https://" + UrlArr(0) + "/formation-en-presentiel/quebec/" + UrlArr(1)
-        'ElseIf Count = 2 Then
-        '    ConcatenatedUrl = "https://" + UrlArr(0) + "/formation-en-presentiel/quebec/" + UrlArr(1) + "/" + UrlArr(2)
-        'ElseIf Count = 3 Then
-        '    ConcatenatedUrl = "https://" + UrlArr(0) + "/formation-en-presentiel/quebec/" + UrlArr(1) + "/" + UrlArr(2) + "/" + UrlArr(3)
-        'End If
-        'For i = 0 To UBound(UrlArr)
-        '    ConcatenatedUrl = UrlArr(i)
-        'Next i
-        
-        'Cell.Offset(0, 5).Value = Split(Cell.Value, "/")
-        Cell.Offset(0, 2).Value = ConcatenatedUrl
-        'Cell.Offset(0, 6).Value = GetPageTitle(ConcatenatedUrl)
-    Next Cell
+    Dim rsSql As New ADODB.Recordset
+
     
+    With connection
+        .ConnectionString = "Provider=SQLNCLI11;Server=" & server_name & _
+            ";database=" & database_name & ";Integrated Security=SSPI;"
+        .ConnectionTimeout = 10
+        .Open
+    End With
+    
+    If connection.State = 1 Then
+        Debug.Print "Connected!"
+    End If
+    
+    rsSql.CursorLocation = adUseClient
+    rsSql.Open sqlQuery, connection, adOpenStatic
+    
+    ThisWorkbook.Sheets("SQLRedirectTable").Range("A2").CopyFromRecordset rsSql
 End Sub
